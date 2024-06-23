7687d9f211d30a42703d7988e0e341462b9b64bc
TimSousa <martimnevessousa@gmail.com>
2024-06-23 03:25:37 +0100

    speakin and listenin, over the net

---
+++ learning/listen.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define N 10
+#define PORT "50607"
+
+#define GREEN_T "\033[1;32m"
+#define RED_T   "\033[1;31m"
+#define RESET_T "\033[0m"
+
+int main(void) {
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    struct addrinfo *res;
+    int ret = getaddrinfo("localhost", PORT, &hints, &res);
+
+    if (ret) {
+        printf(RED_T "%s\n" RESET_T, gai_strerror(ret));
+        return -1;
+    }
+
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (sock == -1) {
+        perror(RED_T "call to socket() failed.\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    int yes = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror(RED_T "call to setsockopt() failed." RESET_T);
+        return -1;
+    } 
+
+    ret = bind(sock, res->ai_addr, res->ai_addrlen);
+    if (ret == -1) {
+        perror(RED_T "call to bind() failed.\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    printf("listening on port %s..\n", PORT);
+    ret = listen(sock, N);
+    if (ret == -1) {
+        perror(RED_T "call to listen() failed.\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    struct sockaddr_storage new_st;
+    struct sockaddr new_sa = *(struct sockaddr*) &new_st;
+
+    socklen_t len = sizeof(new_st);
+
+    int new_fd;
+    new_fd = accept(sock, &new_sa, &len);
+    if (new_fd == -1) {
+        perror(RED_T "call to accept() failed.\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    struct sockaddr_in new_sai = *(struct sockaddr_in *) &new_sa;
+    char ip_out[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &new_sai.sin_addr, ip_out, INET_ADDRSTRLEN)) {
+        printf(GREEN_T "connected to %s!\n" RESET_T, ip_out);
+    }
+
+    char buf[1024];
+    int b = recv(new_fd, buf, sizeof(buf), 0);
+    printf("%s", buf);
+
+    freeaddrinfo(res);
+
+    return 0;
+}
---
+++ learning/speak.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define PORT "50607"
+
+#define GREEN_T "\033[1;32m"
+#define RED_T   "\033[1;31m"
+#define RESET_T "\033[0m"
+
+
+int main(void) {
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    struct addrinfo *res;
+    int ret = getaddrinfo("localhost", PORT, &hints, &res);
+
+    if (ret) {
+        perror(RED_T "call to getaddrinfo() failed.\n" RESET_T);
+        printf("%s\n", gai_strerror(ret));
+        return -1;
+    }
+
+    char l_out[INET_ADDRSTRLEN];
+
+    struct sockaddr *a = res->ai_addr;
+    struct sockaddr_in *a_b = (struct sockaddr_in *) a;
+
+    if (!inet_ntop(res->ai_family, &(a_b->sin_addr), l_out, INET_ADDRSTRLEN)) {
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    printf("found possible connection at %s:%hu\n", l_out, (ushort) ntohs(a_b->sin_port));
+
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (sock == -1) {
+        perror(RED_T "call to socket() failed\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    ret = connect(sock, res->ai_addr, res->ai_addrlen);
+    if (ret == -1) {
+        perror(RED_T "call to connect() failed.\n" RESET_T);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    char msg[] = "testing test";
+    int b = send(sock, msg, sizeof(msg), 0);
+    printf(GREEN_T "sent %d out of %d bytes!\n" RESET_T, b, (int) sizeof(msg));
+
+    freeaddrinfo(res);
+
+    return 0;
+}
