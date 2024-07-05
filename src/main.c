a3d7123fc8142de9816c8354def428c5ec36d654
Peter <peporod12@gmail.com>
2024-07-05 20:30:57 +0100

    Started frmaework for threads

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <pthread.h>
+
 #define PORT_STRLEN 6
 
 #define GREEN_T "\033[1;32m"
@@ -21,48 +23,56 @@ typedef enum {
 	BIND, CONNECT
 } MODE;
 
+typedef enum {
+	READ, WRITE
+} PIPE;
+
+typedef struct {
+	struct addrinfo *target;
+	int sock;
+} thread_args;
 
 void usage(char *name) {
-    printf("Usage: %s --ip=<ip> --port=<port>\n", name);
+	printf("Usage: %s --ip=<ip> --port=<port>\n", name);
 }
 
 
 int process_opts(char *ip, char *port, int argc, char **argv) {
-    int opt;
-    struct option options[2] = {0}; // ip and port
-    options[0].name = "ip";
-    options[0].has_arg = 1;
-    options[0].val = IP;
+	int opt;
+	struct option options[2] = {0}; // ip and port
+	options[0].name = "ip";
+	options[0].has_arg = 1;
+	options[0].val = IP;
 
-    options[1].name = "port";
-    options[1].has_arg = 1;
-    options[1].val = PORT;
+	options[1].name = "port";
+	options[1].has_arg = 1;
+	options[1].val = PORT;
 
-    while ((opt = getopt_long(argc, argv, "", options, NULL)) != -1) {
-        switch (opt) {
-            case IP:
-                strcpy(ip, optarg);
-                break;
-            case PORT:
-                strcpy(port, optarg);
-                break;
-            case '?':
-            default:
-                return -1;
-        }
-    }
+	while ((opt = getopt_long(argc, argv, "", options, NULL)) != -1) {
+		switch (opt) {
+			case IP:
+				strcpy(ip, optarg);
+				break;
+			case PORT:
+				strcpy(port, optarg);
+				break;
+			case '?':
+			default:
+				return -1;
+		}
+	}
 
-    return optind;
+	return optind;
 }
 
 int check_addr(char *ip, char *port, struct addrinfo hints, struct addrinfo *target, char *target_name) {
 	int ret;	
 	if (!target_name) target_name = "Unknown";
 	ret = getaddrinfo(ip, port, &hints, &target);
-    if (ret) {
-        fprintf(stderr, RED_T "getaddrinfo() to %s failed: %s\n" RESET_T, target_name, gai_strerror(ret));
+	if (ret) {
+		fprintf(stderr, RED_T "getaddrinfo() to %s failed: %s\n" RESET_T, target_name, gai_strerror(ret));
 		return -1;
-    }
+	}
 	return 0;
 }
 
@@ -72,7 +82,7 @@ int setup_socket(MODE mode, struct addrinfo *target){
 
 	int sock = socket(target->ai_family, target->ai_socktype, target->ai_protocol);
 	if (sock == -1) {
-        fprintf(stderr, RED_T "socket() failed\n" RESET_T);
+		fprintf(stderr, RED_T "socket() failed\n" RESET_T);
 		return -1;
 	}
 	if (mode == BIND) {
@@ -95,24 +105,35 @@ int setup_socket(MODE mode, struct addrinfo *target){
 	return sock;
 }
 
+void *recieve_thread(void *arg){
+	thread_args *args = (thread_args *) arg;		
+
+	return 0;
+}
+
+void *send_thread(struct addrinfo *target, int sock){
+
+	return 0;
+}
+
 int main(int argc, char **argv){
-    int ret;
-    char ip[INET_ADDRSTRLEN] = {0};
-    char port[PORT_STRLEN] = {0};
+	int ret;
+	char ip[INET_ADDRSTRLEN] = {0};
+	char port[PORT_STRLEN] = {0};
 
-    ret = process_opts(ip, port, argc, argv);
-    if (ret != 3) {
-        printf("%s:%s\n", ip, port);
+	ret = process_opts(ip, port, argc, argv);
+	if (ret != 3) {
+		printf("%s:%s\n", ip, port);
 
-        usage(*argv);
-        return -1;
-    }
+		usage(*argv);
+		return -1;
+	}
 
-    struct addrinfo hints, *local, *peer;
-    memset(&hints, 0, sizeof(hints));
+	struct addrinfo hints, *local, *peer;
+	memset(&hints, 0, sizeof(hints));
 
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
 	//Custom function
@@ -125,6 +146,29 @@ int main(int argc, char **argv){
 	if ((recieve_sock = setup_socket(BIND, local)) == -1) return -1;
 
 	if ((send_sock = setup_socket(CONNECT, peer)) == -1) return -1;
+	
+	pthread_t thread_id[2];
+	thread_args args[2];
+	
+	//Pipes are not needed yet
+	/*
+	int pipes[2][2];
+	
+	for (int i = 0; i < 2; i++) {
+		pipe(pipes[i]);
+	}
+	*/
 
-    return 0;
+
+	args[RECIEVE]->addrinfo = local;
+	args[RECIEVE]->sock = recieve_sock;
+
+	args[SEND]->addrinfo = peer;
+	args[SEND]->sock = send_sock;
+	
+	static int END = 0;
+
+	pthread_create()
+
+	return 0;
 }
